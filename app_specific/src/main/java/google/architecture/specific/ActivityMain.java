b5bc48c7165bea85a08296d0900a1759b924a4b3
zhangyilong <316011989@qq.cm>
2018-01-16 01:46:14 +0800

    基于dawish的项目新建项目 dawish基于官方mvvm实现组件化开发

--- app_specific/src/main/java/google/architecture/specific/ActivityMain.java
+++ app_specific/src/main/java/google/architecture/specific/ActivityMain.java
@@ -1,11 +1,9 @@
 package google.architecture.specific;
 
 import android.databinding.DataBindingUtil;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
-import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.MenuItem;
 
 import com.alibaba.android.arouter.launcher.ARouter;
@@ -16,7 +14,6 @@ import java.util.List;
 import google.architecture.common.base.ARouterPath;
 import google.architecture.common.base.BaseActivity;
 import google.architecture.common.base.BaseFragment;
-import google.architecture.common.util.BindingUtils;
 import google.architecture.common.widget.NoScrollViewPager;
 import google.architecture.specific.databinding.ActivityMainBinding;
 
@@ -51,15 +48,15 @@ public class ActivityMain extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-         mainBinding = DataBindingUtil.setContentView(ActivityMain.this, R.layout.activity_main);
+        mainBinding = DataBindingUtil.setContentView(ActivityMain.this, R.layout.activity_main);
 
-         mainBinding.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener);
-         mPager = mainBinding.containerPager;
-         mPager.setOffscreenPageLimit(3);
+        mainBinding.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener);
+        mPager = mainBinding.containerPager;
+        mPager.setOffscreenPageLimit(3);
 
         BaseFragment fragmentNews = (BaseFragment) ARouter.getInstance().build(ARouterPath.NewsListFgt).navigation();
-        BaseFragment fragmentGirls = (BaseFragment) ARouter.getInstance().build( ARouterPath.GirlsListFgt).navigation();
-        BaseFragment fragmentAbout = (BaseFragment) ARouter.getInstance().build( ARouterPath.AboutFgt ).navigation();
+        BaseFragment fragmentGirls = (BaseFragment) ARouter.getInstance().build(ARouterPath.GirlsListFgt).navigation();
+        BaseFragment fragmentAbout = (BaseFragment) ARouter.getInstance().build(ARouterPath.AboutFgt).navigation();
 
         mFragments.add(fragmentNews);
         mFragments.add(fragmentGirls);
